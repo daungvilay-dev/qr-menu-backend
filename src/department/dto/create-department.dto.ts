d3f45a5a0c14e071b561017b794bfffae6e223d8
daungvilay.m <daungvilay.m@nt1pc.com>
2025-09-21 21:27:19 +0700

    Refactor package.json scripts and enhance DTO validation. Changed 'start:dev' to 'dev' in package.json. Added global validation pipe in main.ts. Expanded CreateDepartmentDto with validation rules for email, password, and code.

--- src/department/dto/create-department.dto.ts
+++ src/department/dto/create-department.dto.ts
@@ -1,1 +1,23 @@
-export class CreateDepartmentDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  Max,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateDepartmentDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(10)
+  password: string;
+
+  @IsNumber()
+  @Max(3)
+  code: number;
+}
